b96fdffcbf6df20a9711ee51f59df923e1bf5d72
motisain25 <105988138+motisain25@users.noreply.github.com>
2023-06-10 15:16:36 +0530

    Add files via upload

---
+++ punctword.py
@@ -0,0 +1,4 @@
+from nltk.tokenize import PunktWordTokenizer
+tokenizer = PunktWordTokenizer()
+tokenizer.tokenize("Let's see how it's working.")
+print(tokenizer.tokenize("Let's see how it's working."))
